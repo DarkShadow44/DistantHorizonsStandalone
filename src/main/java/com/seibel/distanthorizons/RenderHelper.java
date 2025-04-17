33fe78ca021e8f3b54566802c3e684a20c530a01
Fabian Maurer <dark.shadow4@web.de>
2025-04-17 23:29:51 +0200

    test

--- src/main/java/com/seibel/distanthorizons/RenderHelper.java
+++ src/main/java/com/seibel/distanthorizons/RenderHelper.java
@@ -9,39 +9,48 @@ import com.seibel.distanthorizons.interfaces.IMixinMinecraft;
 import net.minecraft.client.Minecraft;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL32;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 public class RenderHelper {
     public static void drawLods()
     {
-        GL32.glDisable(GL32.GL_ALPHA_TEST);
+      //  GL32.glDisable(GL32.GL_ALPHA_TEST);
         Mat4f mcModelViewMatrix = getModelViewMatrix();
         Mat4f mcProjectionMatrix = getProjectionMatrix();
         float frameTime = ((IMixinMinecraft) Minecraft.getMinecraft()).getTimer().renderPartialTicks;
         IClientLevelWrapper levelWrapper = ClientLevelWrapper.getWrapper(Minecraft.getMinecraft().theWorld);
         ClientApi.INSTANCE.renderLods(levelWrapper, mcModelViewMatrix, mcProjectionMatrix, frameTime);
-        GL32.glDepthFunc(GL32.GL_LEQUAL);
+       // GL32.glDepthFunc(GL32.GL_LEQUAL);
     }
 
+    private static Matrix4f modelViewMatrix;
+    private static Matrix4f projectionMatrix;
+
     public static Matrix4f getModelViewMatrixMC() {
-        float[] buffer = new float[16];
-        GL32.glGetFloatv(GL32.GL_MODELVIEW_MATRIX, buffer);
-        return new Matrix4f(FloatBuffer.wrap(buffer));
+        return modelViewMatrix;
     }
 
     public static Matrix4f getProjectionMatrixMC() {
-        float[] buffer = new float[16];
-        GL32.glGetFloatv(GL32.GL_PROJECTION_MATRIX, buffer);
-        return new Matrix4f(FloatBuffer.wrap(buffer));
+        return projectionMatrix;
     }
 
     public static Mat4f getModelViewMatrix() {
-        return McObjectConverter.Convert(getModelViewMatrixMC());
+        return McObjectConverter.Convert(modelViewMatrix);
     }
 
     public static Mat4f getProjectionMatrix() {
-       return McObjectConverter.Convert(getProjectionMatrixMC());
+       return McObjectConverter.Convert(projectionMatrix);
+    }
+
+    public static void setModelViewMatrix(FloatBuffer modelview) {
+        modelViewMatrix = new Matrix4f(modelview);
+    }
+
+    public static void setProjectionMatrix(FloatBuffer projection) {
+        projectionMatrix = new Matrix4f(projection);
     }
 }
