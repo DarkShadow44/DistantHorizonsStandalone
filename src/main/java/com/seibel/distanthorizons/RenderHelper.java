066c2ea42a844ec24d27f67e3e6df372e3b00ec3
Fabian Maurer <dark.shadow4@web.de>
2025-04-18 17:21:13 +0200

    fix crash

--- src/main/java/com/seibel/distanthorizons/RenderHelper.java
+++ src/main/java/com/seibel/distanthorizons/RenderHelper.java
@@ -10,6 +10,7 @@ import net.minecraft.client.Minecraft;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL32;
 
 import java.nio.ByteBuffer;
@@ -18,13 +19,14 @@ import java.nio.FloatBuffer;
 public class RenderHelper {
     public static void drawLods()
     {
-      //  GL32.glDisable(GL32.GL_ALPHA_TEST);
+        GL32.glDisable(GL32.GL_ALPHA_TEST);
         Mat4f mcModelViewMatrix = getModelViewMatrix();
         Mat4f mcProjectionMatrix = getProjectionMatrix();
         float frameTime = ((IMixinMinecraft) Minecraft.getMinecraft()).getTimer().renderPartialTicks;
         IClientLevelWrapper levelWrapper = ClientLevelWrapper.getWrapper(Minecraft.getMinecraft().theWorld);
         ClientApi.INSTANCE.renderLods(levelWrapper, mcModelViewMatrix, mcProjectionMatrix, frameTime);
-       // GL32.glDepthFunc(GL32.GL_LEQUAL);
+        GL32.glDepthFunc(GL32.GL_LEQUAL);
+        GL32.glEnable(GL32.GL_ALPHA_TEST);
     }
 
     private static Matrix4f modelViewMatrix;
@@ -52,5 +54,9 @@ public class RenderHelper {
 
     public static void setProjectionMatrix(FloatBuffer projection) {
         projectionMatrix = new Matrix4f(projection);
+    }
+
+    public static void HelpTess() {
+        GL20.glBindBuffer(GL20.GL_ARRAY_BUFFER, 0);
     }
 }
