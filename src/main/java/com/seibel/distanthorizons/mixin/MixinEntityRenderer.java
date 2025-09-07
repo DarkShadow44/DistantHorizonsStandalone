71f55746c62d45da7efadcd5a68d9d5fd84922a4
Fabian Maurer <dark.shadow4@web.de>
2025-09-07 18:58:52 +0200

    Fix BoP fog

--- src/main/java/com/seibel/distanthorizons/mixin/MixinEntityRenderer.java
+++ src/main/java/com/seibel/distanthorizons/mixin/MixinEntityRenderer.java
@@ -1,14 +1,15 @@
 package com.seibel.distanthorizons.mixin;
 
 import com.seibel.distanthorizons.interfaces.IMixinEntityRenderer;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.EventBus;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.texture.DynamicTexture;
 import org.lwjglx.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer implements IMixinEntityRenderer {
@@ -16,10 +17,12 @@ public abstract class MixinEntityRenderer implements IMixinEntityRenderer {
     @Accessor("lightmapTexture")
     public abstract DynamicTexture getLightmapTexture();
 
-    @Inject(method = "setupFog", at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/eventhandler/EventBus;post(Lcpw/mods/fml/common/eventhandler/Event;)Z", shift = At.Shift.AFTER, remap = false))
-    private void notFine$disableFog(int p_78468_1_, float p_78468_2_, CallbackInfo ci) {
+    @Redirect(method = "setupFog", at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/eventhandler/EventBus;post(Lcpw/mods/fml/common/eventhandler/Event;)Z", remap = false))
+    private boolean notFine$disableFog(EventBus instance, Event event) {
         // Extremely high values cause issues, but 15 mebimeters out should be practically infinite
-        GL11.glFogf(org.lwjgl.opengl.GL11.GL_FOG_START, 1024 * 1024 * 15);
+        GL11.glFogf(GL11.GL_FOG_START, 1024 * 1024 * 15);
         GL11.glFogf(GL11.GL_FOG_END, 1024 * 1024 * 16);
+        GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_LINEAR);
+        return false;
     }
 }
